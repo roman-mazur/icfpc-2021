427b7e18cc63667769930bcb82812254606603f4
jgravier <jean.gravier@ubisoft.com>
2021-07-09 20:31:32 +0200

    Added Problem JSON parsing

--- cmd/starter/main.go
+++ cmd/starter/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "log"
+import (
+	"log"
+	"os"
+)
+
+func fatalUsage() {
+	log.Fatalf("Usage: %s file.problem\n", os.Args[0])
+}
 
 func main() {
 	log.Println("Hello ICFP Contest!")
+	if len(os.Args) < 2 {
+		fatalUsage()
+	}
+
+	// pb := data.ParseProblem(os.Args[1])
 }
--- data/data.go
+++ data/data.go
@@ -1,6 +1,8 @@
 package data
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"math"
 )
@@ -22,6 +24,12 @@ type Figure struct {
 	Edges    []*Edge
 }
 
+type Problem struct {
+	Hole    *Hole   `json:"hole"`
+	Figure  *Figure `json:"figure"`
+	Epsilon int     `json:"epsilon"`
+}
+
 func (e *Edge) SqLength() float64 {
 	return math.Pow(float64(e.A.X-e.B.X), 2) + math.Pow(float64(e.A.Y-e.B.Y), 2)
 }
@@ -36,8 +44,64 @@ func (e *Edge) Line() (k, b float64) {
 		k = 0
 		b = float64(e.A.Y)
 	} else {
-		k = float64(e.A.Y - e.B.Y) / float64(e.A.X - e.B.X)
+		k = float64(e.A.Y-e.B.Y) / float64(e.A.X-e.B.X)
 		b = float64(e.A.Y) - k*float64(e.A.X)
 	}
 	return
+}
+
+func (v *Vertice) UnmarshalJSON(b []byte) error {
+	var rawVtx []int
+	if err := json.Unmarshal(b, &rawVtx); err != nil {
+		return err
+	}
+
+	if len(rawVtx) != 2 {
+		return errors.New("invalid vertex")
+	}
+
+	v.X = rawVtx[0]
+	v.Y = rawVtx[1]
+
+	return nil
+}
+
+func (h *Hole) UnmarshalJSON(b []byte) error {
+	var vertices []*Vertice
+	if err := json.Unmarshal(b, &vertices); err != nil {
+		return err
+	}
+
+	h.Vertices = vertices
+	return nil
+}
+
+func (f *Figure) UnmarshalJSON(b []byte) error {
+	var rawFigure map[string][]*Vertice
+	if err := json.Unmarshal(b, &rawFigure); err != nil {
+		return err
+	}
+
+	edges, ok := rawFigure["edges"]
+	if !ok {
+		return errors.New("invalid edges")
+	}
+
+	vertices, ok := rawFigure["vertices"]
+	if !ok {
+		return errors.New("invalid vertices")
+	}
+
+	for i := 0; i < len(edges); i++ {
+		if i%2 == 0 {
+			f.Edges = append(f.Edges, &Edge{
+				A: edges[i],
+			})
+		} else {
+			f.Edges[i/2].B = edges[i]
+		}
+	}
+
+	f.Vertices = vertices
+	return nil
 }
