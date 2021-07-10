690b0e1270998b2bada0ca09bb0599970d0ff4e7
Roman Mazur <mazur.roman@gmail.com>
2021-07-10 12:55:05 +0200

    Unify drawing

--- gfx/draw.go
+++ gfx/draw.go
@@ -1,15 +1,11 @@
 package gfx
 
 import (
-	"fmt"
-
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/imdraw"
 	"github.com/faiface/pixel/pixelgl"
-	"github.com/faiface/pixel/text"
 	"github.com/roman-mazur/icfpc-2021/data"
 	"golang.org/x/image/colornames"
-	"golang.org/x/image/font/basicfont"
 )
 
 var k = 6.0 // temp scale
@@ -24,53 +20,29 @@ func DrawProblem(cfg pixelgl.WindowConfig, pb *data.Problem) {
 		for !win.Closed() {
 			win.Clear(colornames.Gray)
 
-			drawHole(win, pb.Hole)
-			drawFigure(win, pb.Figure.Edges)
+			drawHole(pb.Hole).Draw(win)
+			drawFigure(pb.Figure).Draw(win)
 
 			win.Update()
 		}
 	})
 }
 
-func drawHole(win *pixelgl.Window, h *data.Hole) {
-	imd, _ := newIMDrawWithEdges(h.Edges, false)
-
+func drawHole(hole *data.Hole) *imdraw.IMDraw {
+	imd := imdraw.New(nil)
 	imd.SetColorMask(pixel.RGB(0.8, 0.8, 0.8))
-	imd.Line(2)
-	imd.Draw(win)
+	drawEdges(imd, hole.Edges, 2)
+	return imd
 }
 
-func drawFigure(win *pixelgl.Window, edges []*data.Edge) {
-	imd, _ := newIMDrawWithFigureEdges(edges, false)
-	imd.Draw(win)
-}
-
-func newIMDrawWithEdges(edges []*data.Edge, idx bool) (*imdraw.IMDraw, *text.Text) {
+func drawFigure(f *data.Figure) *imdraw.IMDraw {
 	imd := imdraw.New(nil)
-
-	for _, e := range edges {
-		imd.Push(
-			pixel.V(
-				float64(e.A.X)*k,  // x1
-				float64(e.A.Y)*k), // y1
-			pixel.V(
-				float64(e.B.X)*k,  // x2
-				float64(e.B.Y)*k), // y2
-		)
-	}
-
-	atlas := text.NewAtlas(basicfont.Face7x13, text.ASCII)
-	txt := text.New(pixel.V(50, 50), atlas)
-
-	fmt.Fprintf(txt, "hello, world%s", "!")
-
-	return imd, txt
-}
-
-func newIMDrawWithFigureEdges(edges []*data.Edge, idx bool) (*imdraw.IMDraw, *text.Text) {
-	imd := imdraw.New(nil)
-
 	imd.SetColorMask(pixel.RGB(1.0, 0, 0))
+	drawEdges(imd, f.Edges, 5)
+	return imd
+}
+
+func drawEdges(imd *imdraw.IMDraw, edges []*data.Edge, thickness float64) {
 	for _, e := range edges {
 		imd.Push(
 			pixel.V(
@@ -80,14 +52,7 @@ func newIMDrawWithFigureEdges(edges []*data.Edge, idx bool) (*imdraw.IMDraw, *te
 				float64(e.B.X)*k,  // x2
 				float64(e.B.Y)*k), // y2
 		)
-		imd.Line(5)
+		imd.Line(thickness)
 		imd.Reset()
 	}
-
-	atlas := text.NewAtlas(basicfont.Face7x13, text.ASCII)
-	txt := text.New(pixel.V(50, 50), atlas)
-
-	fmt.Fprintf(txt, "hello, world%s", "!")
-
-	return imd, txt
 }
