e87cbb63cee2486618eb3c15ddbc58c9c5d42c61
Jean Gravier <jean.gravier@pm.me>
2021-07-10 12:14:42 +0200

    WIP text

--- gfx/draw.go
+++ gfx/draw.go
@@ -1,11 +1,15 @@
 package gfx
 
 import (
+	"fmt"
+
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/imdraw"
 	"github.com/faiface/pixel/pixelgl"
+	"github.com/faiface/pixel/text"
 	"github.com/roman-mazur/icfpc-2021/data"
 	"golang.org/x/image/colornames"
+	"golang.org/x/image/font/basicfont"
 )
 
 var k = 6.0 // temp scale
@@ -29,7 +33,7 @@ func DrawProblem(cfg pixelgl.WindowConfig, pb *data.Problem) {
 }
 
 func drawHole(win *pixelgl.Window, h *data.Hole) {
-	imd := newIMDrawWithEdges(h.Edges)
+	imd, _ := newIMDrawWithEdges(h.Edges, false)
 
 	imd.SetColorMask(pixel.RGB(0.8, 0.8, 0.8))
 	imd.Line(2)
@@ -37,14 +41,14 @@ func drawHole(win *pixelgl.Window, h *data.Hole) {
 }
 
 func drawFigure(win *pixelgl.Window, edges []*data.Edge) {
-	imd := newIMDrawWithEdges(edges)
+	imd, _ := newIMDrawWithEdges(edges, false)
 
 	imd.SetColorMask(pixel.RGB(1.0, 0, 0))
 	imd.Line(5)
 	imd.Draw(win)
 }
 
-func newIMDrawWithEdges(edges []*data.Edge) *imdraw.IMDraw {
+func newIMDrawWithEdges(edges []*data.Edge, idx bool) (*imdraw.IMDraw, *text.Text) {
 	imd := imdraw.New(nil)
 
 	for _, e := range edges {
@@ -58,5 +62,10 @@ func newIMDrawWithEdges(edges []*data.Edge) *imdraw.IMDraw {
 		)
 	}
 
-	return imd
+	atlas := text.NewAtlas(basicfont.Face7x13, text.ASCII)
+	txt := text.New(pixel.V(50, 50), atlas)
+
+	fmt.Fprintf(txt, "hello, world%s", "!")
+
+	return imd, txt
 }
