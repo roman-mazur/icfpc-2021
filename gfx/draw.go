6243e71bb0a194ad90ebf42a03dcf87d935e8b99
Jean Gravier <jean.gravier@pm.me>
2021-07-10 14:01:36 +0200

    Added Edge number. Revert window matrix inversion

--- gfx/draw.go
+++ gfx/draw.go
@@ -1,16 +1,20 @@
 package gfx
 
 import (
+	"fmt"
+
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/imdraw"
 	"github.com/faiface/pixel/pixelgl"
+	"github.com/faiface/pixel/text"
 	"github.com/roman-mazur/icfpc-2021/data"
 	"golang.org/x/image/colornames"
+	"golang.org/x/image/font/basicfont"
 )
 
 var (
 	k         = 6.0 // temp scale
-	marginTop = 6.0
+	posMatrix = pixel.IM.Moved(pixel.V(0, -5))
 )
 
 func DrawProblem(cfg pixelgl.WindowConfig, pb *data.Problem) {
@@ -20,23 +24,12 @@ func DrawProblem(cfg pixelgl.WindowConfig, pb *data.Problem) {
 			panic(err)
 		}
 
-		// Trick to invert origin
-		win.SetMatrix(
-			pixel.IM.ScaledXY(
-				pixel.V(0, 0),
-				pixel.V(1, -1),
-			).Chained(
-				pixel.IM.Moved(
-					pixel.V(0, win.Bounds().H()+marginTop),
-				),
-			),
-		)
-
 		for !win.Closed() {
 			win.Clear(colornames.Gray)
 
 			drawHole(pb.Hole).Draw(win)
 			drawFigure(pb.Figure).Draw(win)
+			drawEdgeNums(win, pb.Figure.Edges)
 
 			win.Update()
 		}
@@ -45,6 +38,7 @@ func DrawProblem(cfg pixelgl.WindowConfig, pb *data.Problem) {
 
 func newDraw() *imdraw.IMDraw {
 	res := imdraw.New(nil)
+	res.SetMatrix(posMatrix)
 	return res
 }
 
@@ -72,7 +66,24 @@ func drawEdges(imd *imdraw.IMDraw, edges []*data.Edge, thickness float64) {
 				float64(e.B.X)*k,  // x2
 				float64(e.B.Y)*k), // y2
 		)
+
 		imd.Line(thickness)
 		imd.Reset()
+	}
+}
+
+func drawEdgeNums(win *pixelgl.Window, edges []*data.Edge) {
+	atlas := text.NewAtlas(basicfont.Face7x13, text.ASCII)
+
+	for i, e := range edges {
+		line := pixel.L(
+			pixel.V(float64(e.A.X)*k, float64(e.A.Y)*k),
+			pixel.V(float64(e.B.X)*k, float64(e.B.Y)*k),
+		)
+
+		txt := text.New(line.Center(), atlas)
+		fmt.Fprintf(txt, "%d", i)
+
+		txt.Draw(win, pixel.IM)
 	}
 }
