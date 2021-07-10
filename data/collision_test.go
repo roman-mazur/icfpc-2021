1ab426b34b74c73015c45797ba804e7ee6054fc9
Luka <luka+git@geluti.org>
2021-07-10 13:29:21 +0200

    Fix hole.Contains algorithm

--- data/collision_test.go
+++ data/collision_test.go
@@ -12,16 +12,17 @@ const (
 	scale   = 1
 )
 
+// Collection of pre-defined vertices for testing
 var (
 	v00 = Vertex{X: 0, Y: 0}
 	v01 = Vertex{X: 0, Y: 1}
-	v11 = Vertex{X: 1, Y: 1}
-	v10 = Vertex{X: 1, Y: 0}
-	v22 = Vertex{X: 2, Y: 2}
 	v02 = Vertex{X: 0, Y: 2}
+	v10 = Vertex{X: 1, Y: 0}
+	v11 = Vertex{X: 1, Y: 1}
+	v12 = Vertex{X: 1, Y: 2}
 	v20 = Vertex{X: 2, Y: 0}
 	v21 = Vertex{X: 2, Y: 1}
-	v12 = Vertex{X: 1, Y: 2}
+	v22 = Vertex{X: 2, Y: 2}
 )
 
 func newEdge(A, B Vertex) Edge {
@@ -43,9 +44,11 @@ func TestIntersect(t *testing.T) {
 		test{newEdge(v01, v21), newEdge(v11, v10), true},
 		test{newEdge(v01, v21), newEdge(v11, v12), true},
 		test{newEdge(v00, v20), newEdge(v20, v22), true},
+		test{newEdge(v10, v20), newEdge(v10, v20), true}, // Collinear, but share points
 		test{newEdge(v00, v10), newEdge(v02, v20), false},
 		test{newEdge(v01, v10), newEdge(v11, v21), false},
 		test{newEdge(v10, v10), newEdge(v02, v20), false}, // Collinear
+		test{newEdge(Vertex{-1000000, 0}, v00), newEdge(v12, v01), false},
 	}
 
 	for _, test := range suite {
@@ -78,19 +81,43 @@ func TestContains(t *testing.T) {
 		expect bool
 	}
 
-	hole := Hole{
-		Vertices: []Vertex{v10, v11, v12, v01},
+	square := Hole{
+		Vertices: []Vertex{
+			Vertex{10, 0},
+			Vertex{20, 10},
+			Vertex{10, 20},
+			Vertex{0, 10},
+		},
 	}
-	hole.FillEdges()
+	square.FillEdges()
+
+	concave := Hole{
+		Vertices: []Vertex{
+			Vertex{0, 0},
+			Vertex{30, 30},
+			Vertex{60, 0},
+			Vertex{60, 60},
+			Vertex{0, 60},
+		},
+	}
+	concave.FillEdges()
 
 	var suite = []test{
-		test{hole, v11, true},
-		test{hole, v00, false},
+		test{square, Vertex{10, 10}, true},
+		test{square, Vertex{5, 5}, true},
+		test{square, Vertex{10, 0}, true},
+		test{square, v00, false},
+
+		test{concave, Vertex{6, 20}, true},
+		test{concave, Vertex{50, 40}, true},
+		test{concave, Vertex{30, 10}, false},
+		test{concave, Vertex{10, 75}, false},
+		test{concave, Vertex{75, 10}, false},
 	}
 
 	for _, test := range suite {
 		t.Run(
-			fmt.Sprintf("hole-contains-%d%d-%v", test.vertex.X, test.vertex.Y, test.expect),
+			fmt.Sprintf("hole-contains-(%d,%d)-%v", test.vertex.X, test.vertex.Y, test.expect),
 			func(t *testing.T) {
 				assert.Assert(t, test.hole.Contains(test.vertex) == test.expect)
 			},
