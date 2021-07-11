dfc5fcaea66229d68c08009d1ed6195fa5698cf2
Roman Mazur <mazur.roman@gmail.com>
2021-07-11 13:04:03 +0200

    Fix rebase

--- data/valid_test.go
+++ data/valid_test.go
@@ -22,20 +22,20 @@ func TestScaleToPreserveRatio(t *testing.T) {
 	} {
 		{
 			name: "equal",
-			n: &Edge{A: &Vertex{1, 1}, B: &Vertex{1, 3}},
-			o: &Edge{A: &Vertex{2, 2}, B: &Vertex{2, 4}},
+			n: &Edge{A: &Vertex{X: 1, Y: 1}, B: &Vertex{X: 1, Y: 3}},
+			o: &Edge{A: &Vertex{X: 2, Y: 2}, B: &Vertex{X: 2, Y: 4}},
 			eps: 1,
 		},
 		{
 			name: "too big",
-			n: &Edge{A: &Vertex{2, 2}, B: &Vertex{2, 6}},
-			o: &Edge{A: &Vertex{1, 1}, B: &Vertex{1, 3}},
+			n: &Edge{A: &Vertex{X: 2, Y: 2}, B: &Vertex{X: 2, Y: 6}},
+			o: &Edge{A: &Vertex{X: 1, Y: 1}, B: &Vertex{X: 1, Y: 3}},
 			eps: 2e5,
 		},
 		{
 			name: "too small",
-			n: &Edge{A: &Vertex{1, 1}, B: &Vertex{1, 3}},
-			o: &Edge{A: &Vertex{2, 2}, B: &Vertex{2, 6}},
+			n: &Edge{A: &Vertex{X: 1, Y: 1}, B: &Vertex{X: 1, Y: 3}},
+			o: &Edge{A: &Vertex{X: 2, Y: 2}, B: &Vertex{X: 2, Y: 6}},
 			eps: 5e5,
 		},
 	} {
